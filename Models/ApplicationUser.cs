5558ca900aaa70757ec54cd86877407914bf4ac8
Baptiste <baptiste.asselin@orange.fr>
2026-05-22 08:26:14 +0200

    entities annotations

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace GradeManager.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "First name cannot be longer than 100 characters.")]
         public string? FirstName { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Last name cannot be longer than 100 characters.")]
         public string? LastName { get; set; }
     }
 }
--- Models/Grade.cs
+++ Models/Grade.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GradeManager.Models
 {
     public class Grade
     {
+        [Key]
         public string Id { get; init; } = Guid.NewGuid().ToString();
         
+        [Required]
+        [Range(0, 20, ErrorMessage = "Grade value must be between 0 and 20.")]
         public required int Value { get; set; }
 
+        [Required]
+        [Range(0, 10, ErrorMessage = "Coefficient must be between 0 and 10.")]
         public required float Coefficient { get; set; }
 
+        [Required]
         public required string StudentId { get; set; }
+
+        [Required]
         public virtual required Student Student { get; set; }
     }
 }
